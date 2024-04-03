c28a21f91249ba32ba6e8f23af10059738880357
Davidevk2 <k2guffy@gmail.com>
2024-04-02 19:29:45 -0500

    Conexion controller con dbContext

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using Solucion.Data;
 
 namespace Solucion.Controllers
 {
     public class ProductsController : Controller
     {
+        /* Conexion con la db */
+        public readonly BaseContext _context;
+
+        /* Constructor de productos */
+        public ProductsController(BaseContext context){
+            _context = context;
+        }
         
     }
 }
--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Solucion.Data;
 
 namespace Solucion.Controllers
 {
     
     public class UsersController : Controller
     {
-        
+        /* Conexion con la db */
+        public readonly BaseContext _context;
+        /* Constructor Usuarios */
+        public UsersController(BaseContext context){
+            _context = context;
+        }
     }
 }
