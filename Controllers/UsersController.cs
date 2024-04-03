943071fc173ae0e2b6386aa4ac95a916630c7eda
Davidevk2 <k2guffy@gmail.com>
2024-04-03 15:23:53 -0500

    Creacion de vista para crear users

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -14,12 +14,18 @@ namespace Solucion.Controllers
             _context = context;
         }
 
+        /* Actions para las vistas  */
         public async Task<IActionResult> Index(){
             return View(await _context.Users.ToListAsync());
         }
 
         public async Task<IActionResult> Details(int? id){
             return View(await _context.Users.FirstOrDefaultAsync(user => user.Id == id));
+            /* SELECT * from Users WHERE Id = id */
+        }
+
+        public IActionResult Create(){
+            return View();
         }
     }
 }
