ef995a3a9b4832dd5cffd296d3ee2eed1aa6200a
Davidevk2 <k2guffy@gmail.com>
2024-04-02 19:23:41 -0500

    Definicion de modelos

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,6 +3,14 @@ namespace Solucion.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public double Price {get; set;}
+        public int Amount {get; set;}
+        public string ExpirationDate {get; set;}
+
+        public int UserId {get; set;}
         
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,13 @@ namespace Solucion.Models
 {
     public class User
     {
-        
+        public int Id { get; set; }
+        public string Names { get; set; }
+        public string LastNames { get; set; }
+        public string Email { get; set; }
+        public string BirthDate { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
     }
 }
